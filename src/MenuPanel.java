792763fa56967576f79948a51922dd145d37dd24
윤민서 <cadam88@naver.com>
2024-06-13 19:26:03 +0900

    QR 결제 API 가져와서 구현 필요

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -10,94 +10,93 @@ import java.io.InputStreamReader;
 import javax.swing.*;
 
 
-public class MenuPanel extends JPanel{ //JPanel을 상속받아 패널형식 클래스 만들기
+public class MenuPanel extends JPanel{
 
-    JPanel[] panel;//제품 패널 레퍼런스
-    JPanel[] label;//제품 라벨 레퍼런스
-    JLabel[] productName;//제품 이름 레퍼런스
-    JLabel[] productPrice;//제품 가격 레퍼런스
-    ImageIcon[] productImage;//버튼 이미지 레퍼런스
-    FileInputStream nameFile;
-    FileInputStream priceFile;
-    public JButton[] productButton;//제품 버튼 레퍼런스
-    public String[] name;//제품 이름 저장 배열
-    public String[] price; //제품 가격 저장 배열
-    public String[] imageName; //제품 이름에 따른 이미지파일 이름
+    JPanel[] panel; // 제품 패널
+    JPanel[] label; // 제품 라벨
+    JLabel[] productName; // 제품 이름
+    JLabel[] productPrice; // 제품 가격
+    ImageIcon[] productImage; // 제품 이미지
+    FileInputStream nameFile; // 제품 이름 저장된 파일
+    FileInputStream priceFile; // 제품 가격 저장된 파일
+    public JButton[] productButton; // 제품 버튼
+    public String[] name; // 제품 이름 저장될 배열
+    public String[] price; // 제품 가격 저장될 배열
+    public String[] imageName; // 제품 이름에 맞는 이미지 파일 이름
     String nameTemp;
     String priceTemp;
     int i = 0;
     int j=0;
 
-    //MenuOption[] dialog;
-
-
     public MenuPanel(int count, String nameFile, String priceFile) {
         //제품 패널 생성
-        panel = new JPanel[count];//제품의 개수만큼 제품 패널 레퍼런스 선언
-        label=new JPanel[count];//제품의 개수 만큼 제품의 이름과 가격을 표시할 라벨 레퍼런스 선언
-        productName = new JLabel[count];//제품의 개수만큼 제품 패널의 라벨 레퍼런스 선언
-        productPrice = new JLabel[count];//제품의 개수만큼 제품 패널의 라벨 레퍼런스 선언
-        productButton = new JButton[count];//제품의 개수만큼 제품 패널의 버튼 레퍼런스 선언
-        productImage = new ImageIcon[count];//제품의 개수만큼 버튼의 이미지 레퍼런스 선언
-        name = new String[count];//파일에서 제품의 개수만큼 제품 이름을 받을 스트링  배열 생성
+        panel = new JPanel[count]; // 제품의 개수만큼 제품 패널 배열 선언
+        label=new JPanel[count]; // 제품의 개수 만큼 제품의 이름과 가격을 표시하는 라벨 배열 선언
+        productName = new JLabel[count]; // 제품의 개수만큼 제품 패널의 라벨 배열 선언
+        productPrice = new JLabel[count]; // 제품의 개수만큼 제품 패널의 라벨 배열 선언
+        productButton = new JButton[count]; // 제품의 개수만큼 제품 패널의 버튼 배열 선언
+        productImage = new ImageIcon[count]; // 제품의 개수만큼 버튼의 이미지 배열 선언
+        name = new String[count]; // 파일에서 제품의 개수만큼 제품 이름을 받을 스트링 배열 생성
+        price = new String[count]; // 파일에서 제품의 개수만큼 제품 가격을 받을 스트링 배열 생성
         imageName = new String[count];
-        price = new String[count];//파일에서 제품의 개수만큼 제품 가격을 받을 스트링 배열 생성
+
 
 
         setLayout(null);
-        setBackground(Color.WHITE);//패널의 배경색 흰색
+        setBackground(Color.WHITE);
 
         try {
-            this.nameFile = new FileInputStream("files/" + nameFile + ".txt");//제품 이름 파일을 찾아서 file_n객체 생성
-            InputStreamReader in = new InputStreamReader(this.nameFile, "UTF-8");//file_n객체를 읽을 수 있는 in객체를 생성
-            BufferedReader reader = new BufferedReader(in);//in객체를 버퍼스트림객체로 생성
+            this.nameFile = new FileInputStream("files/" + nameFile + ".txt"); // 제품 이름 파일을 찾아서 nameFile객체 생성
+            InputStreamReader in = new InputStreamReader(this.nameFile, "UTF-8"); // nameFile 객체를 읽을 InputStreamReader 객체 생성
+            BufferedReader reader = new BufferedReader(in); // in객체로 BufferedReader 객체 생성
 
-            //파일 입출력
+            //파일명에 따른 형태 변환
             while((nameTemp = reader.readLine())!=null){
                 name[i]= nameTemp;
                 imageName[i] = nameTemp.replace(' ', '_');
                 i++;
             }
             in.close();
+            reader.close();
         }
         catch(IOException e) {
             System.out.println(e);
         }
 
         try {
-            this.priceFile = new FileInputStream("files/" + priceFile + ".txt");//생성자에서 받은 제품 가격 파일을 찾아서 file_p객체 생성
-            InputStreamReader in = new InputStreamReader(this.priceFile, "UTF-8");//file_p객체를 읽을 수 있는 in객체를 생성
-            BufferedReader reader = new BufferedReader(in);//in객체를 버퍼스트림객체로 생성
+            this.priceFile = new FileInputStream("files/" + priceFile + ".txt"); // 제품 이름 파일을 찾아서 priceFile객체 생성
+            InputStreamReader in = new InputStreamReader(this.priceFile, "UTF-8"); // priceFile 객체를 읽을 InputStreamReader 객체 생성
+            BufferedReader reader = new BufferedReader(in); // in객체로 BufferedReader 객체 생성
 
-            //파일에서 내용이 없을때까지 한줄씩 읽은다음 pricetmp에 입력.
             while ((priceTemp = reader.readLine()) != null) {
                 price[j] = priceTemp;
                 j++;
             }
             in.close();
+            reader.close();
         }
         catch(IOException e) {
             System.out.println(e);
         }
 
         for(int i=0; i<count; i++) {
-            panel[i]=new JPanel();//제품 패널 객체 생성
-            panel[i].setLayout(new BorderLayout());//BorderLayout 설정
+            panel[i]=new JPanel(); // 제품 패널 객체
+            panel[i].setLayout(new BorderLayout());
 
-            label[i]=new JPanel();//제품 라벨 패널 생성
-            label[i].setLayout(new GridLayout(1,2));//위치 지정
-            label[i].setBackground(Color.WHITE);//배경색을 흰색으로
-            productName[i]=new JLabel(name[i]);//제품 이름 라벨
-            productPrice[i]=new JLabel(price[i]+"원");//제품 가격 라벨
-            label[i].add(productName[i]);//제품 라벨 패널에 이름 라벨 넣기
-            label[i].add(productPrice[i]);//제품 라벨 패널에 가격 라벨 넣기
-            productImage[i] = new ImageIcon("image/"+imageName[i]+".png");//images파일에서 제품 이름의 이미지를 찾기
+            label[i]=new JPanel(); //제품 라벨 패널
+            label[i].setLayout(new GridLayout(1,2));
+            label[i].setBackground(Color.WHITE);
+            productName[i]=new JLabel(name[i]); // 제품 이름 라벨
+            productPrice[i]=new JLabel(price[i]+"원"); // 제품 가격 라벨
+            label[i].add(productName[i]); // 제품 라벨 패널에 이름 추가
+            label[i].add(productPrice[i]); // 제품 라벨 패널에 가격 추가
+            productImage[i] = new ImageIcon("image/"+imageName[i]+".png"); // image파일에서 제품 이름에 맞는 이미지 찾기
 
-            productButton[i]=new JButton(productImage[i]);//제품의 이미지를 넣어서 버튼 객체 생성
-            productButton[i].setBackground(Color.WHITE);//버튼색 흰색
-            panel[i].add(productButton[i],BorderLayout.CENTER);//CENTER에 버튼 삽입
-            panel[i].add(label[i],BorderLayout.SOUTH);//SOUTH에 제품 라벨 삽입
-            panel[i].setBackground(Color.WHITE);//배경색 흰색
+            productButton[i]=new JButton(productImage[i]); // 제품의 이미지로 버튼 생성
+            productButton[i].setBackground(Color.WHITE);
+            panel[i].add(productButton[i],BorderLayout.CENTER);
+            panel[i].add(label[i],BorderLayout.SOUTH);
+            panel[i].setBackground(Color.WHITE);
             panel[i].setBounds(350*(i%3),350*(i/3),350,350);
             add(panel[i]);
         }
--- src/Request.java
+++ src/Request.java
@@ -5,64 +5,64 @@ import javax.swing.table.DefaultTableModel;
 
 public class Request extends JPanel{
 
-    String []head = {"제품명", "수량", "가격"};//테이블 헤더
-    String [][]data= {};//테이블 형식
+    String []head = {"제품명", "수량", "가격"}; // 테이블 헤더
+    String [][]data= {}; // 테이블 형태로 저장
 
-    public DefaultTableModel table = new DefaultTableModel(data,head);//테이블 모델 생성
-    public JTable cart = new JTable(table);//장바구니 테이블 객체 생성
-    public JButton menuDelete = new JButton("선택한 메뉴 삭제");//장바구니 삭제 버튼
+    public DefaultTableModel table = new DefaultTableModel(data,head); // 테이블 모델 생성
+    public JTable cart = new JTable(table); // 장바구니 테이블 객체 생성
+    public JButton menuDelete = new JButton("선택한 메뉴 삭제"); // 장바구니에서 메뉴 삭제 버튼
 
-    JLabel payLabel = new JLabel("결제 금액");//결제 금액 라벨 객체 생성
-    public JLabel paySum = new JLabel(" ");//금액이 표시될 라벨 객체 생성
+    JLabel payLabel = new JLabel("결제 금액"); // 결제 금액 라벨
+    public JLabel paySum = new JLabel(" "); // 금액이 표시될 라벨
 
-    JLabel requestLabel = new JLabel("요청 사항");//"요청 사항 라벨
-    public JTextArea requestWrite = new JTextArea(); //요청 사항 텍스트
+    JLabel requestLabel = new JLabel("요청 사항"); // 요청 사항 라벨
+    public JTextArea requestWrite = new JTextArea(); // 요청 사항적는 JTestArea
 
-    public JButton order = new JButton("주문");//주문 버튼
-    public JButton cancel = new JButton("취소");//취소 버튼
+    public JButton order = new JButton("주문"); // 주문 버튼
+    public JButton cancel = new JButton("취소"); // 취소 버튼
 
-    public JButton choicePay1 = new JButton("QR 결제");//카드 결제 버튼
-    public JButton choicePay2 = new JButton("현금 결제");//현금 결제 버튼
+    public JButton choicePay1 = new JButton("QR 결제"); // QR 결제 버튼
+    public JButton choicePay2 = new JButton("현금 결제"); // 현금 결제 버튼
 
-    public int allPrice = 0;//전체 금액 0으로 초기화
+    public int allPrice = 0; // 총 금액의 초기값은 0
 
     public Request() {
         setLayout(null);
-        JScrollPane scrollBusket = new JScrollPane(cart);//장바구니 스크롤
-        scrollBusket.setBounds(0,0,525,135);//스크롤 위치,크기
-        add(scrollBusket);//스크롤 넣기
+        JScrollPane scrollBusket = new JScrollPane(cart); // 장바구니 스크롤
+        scrollBusket.setBounds(0,0,525,135);
+        add(scrollBusket);
 
-        requestLabel.setBounds(525,0,525,20);//요청사항라벨
+        requestLabel.setBounds(525,0,525,20); // 요청 사항 라벨
         add(requestLabel);
         requestWrite.setFont(new Font("맑은 고딕",Font.BOLD,22));
-        JScrollPane scrollRequest = new JScrollPane(requestWrite);//요청 사항 스크롤 생성
-        scrollRequest.setBounds(525,20,525,110);//스크롤 텍스트 위치,크기
+        JScrollPane scrollRequest = new JScrollPane(requestWrite);
+        scrollRequest.setBounds(525,20,525,110);
         add(scrollRequest);
 
-        choicePay1.setBounds(525,130,262,30);//카드 결제 버튼
+        choicePay1.setBounds(525,130,262,30); //QR 결제 버튼
         add(choicePay1);
-        choicePay2.setBounds(788,130,262,30);//현금 결제 버튼
+        choicePay2.setBounds(788,130,262,30); // 현금 결제 버튼
         add(choicePay2);
 
-        order.setFont(new Font("맑은 고딕",Font.BOLD,50));//주문 글자 크기
-        order.setBackground(Color.RED);//주문 색깔
+        order.setFont(new Font("맑은 고딕",Font.BOLD,50)); // 주문 글자 크기
+        order.setBackground(Color.RED);
         order.setBounds(525,160,262,85);
         add(order);
 
-        cancel.setFont(new Font("맑은 고딕",Font.BOLD,50));//취소 글자 크기
-        cancel.setBackground(Color.GRAY);//취소 색깔
+        cancel.setFont(new Font("맑은 고딕",Font.BOLD,50)); // 취소 글자 크기
+        cancel.setBackground(Color.GRAY);
         cancel.setBounds(787,160,262,85);
         add(cancel);
 
         menuDelete.setBounds(0,130,525,30);
-        add(menuDelete);//삭제 버튼 삽입
+        add(menuDelete);
 
-        payLabel.setBounds(0,150,150,100);//결제 금액 라벨
-        payLabel.setFont(new Font("맑은 고딕",Font.BOLD,33));//글자 폰트
+        payLabel.setBounds(0,150,150,100); // 결제 금액 라벨
+        payLabel.setFont(new Font("맑은 고딕",Font.BOLD,33));
         add(payLabel);
 
-        paySum.setBounds(150,160,375,81);//금액이 표시될 라벨
-        paySum.setFont(new Font("맑은 고딕",Font.BOLD,57));//금액 글자 폰트
+        paySum.setBounds(150,160,375,81); // 금액이 표시될 라벨
+        paySum.setFont(new Font("맑은 고딕",Font.BOLD,57));
         paySum.setBackground(Color.white);//배경색 설정
         paySum.setOpaque(true);
         add(paySum);
